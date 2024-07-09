5359e715bd469d1d76c6c7558387c6556d3a112c
vedant Ingale <vedantingale@vedants-MacBook-Air.local>
2024-07-09 13:02:23 +0530

    mongo db online server

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // const mongoURL = 'mongodb://localhost:27017/hotels'
 const mongoURL = 'mongodb+srv://ingalevedant28:HelloWorld@cluster0.2quge6s.mongodb.net/'
